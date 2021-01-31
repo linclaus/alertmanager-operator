b65df21dfb9f924401309558c503082580a465da
linclaus <linclaus@outlook.com>
2021-01-31 21:56:55 +0800

    build

--- build/build.sh
+++ build/build.sh
@@ -4,3 +4,4 @@ set -x
 CI_COMMIT_TAG=$(git describe --always --tags)
 
 docker build -t linclaus/alertmanager-operator:latest -f build/package/Dockerfile .
+docker push linclaus/alertmanager-operator:latest
